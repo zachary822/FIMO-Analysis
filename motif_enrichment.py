8341bbb3285a8f6eb18a86b519949b757c6bac57
Zachary Juang <zachary822@me.com>
2018-10-02 12:19:43 -0400

    Include background
    Use an external gene list as background to filter all results

--- motif_enrichment.py
+++ motif_enrichment.py
@@ -1,6 +1,6 @@
 import sys
 from itertools import zip_longest
-from typing import List
+from typing import Generator
 
 import pandas as pd
 from scipy.stats import fisher_exact
@@ -23,13 +23,30 @@ def probability(s):
     return value
 
 
-def get_lists(path: str) -> List[pd.Series]:
-    data = pd.read_csv(path, header=None, squeeze=True)
+def get_lists(path: str) -> Generator[pd.Series, None, None]:
+    with open(path) as f:
+        data = pd.Series(f.readlines())
     data = data.str.strip()
 
-    names = data.index[data.str.startswith(">")]
+    names = data.index[data.str.startswith(">")].tolist()
 
-    return [data[start + 1:end].rename(data[start].strip(">")) for start, end in zip_longest(names, names[1:])]
+    if not names:
+        names = [-1]
+    elif names[0] > 0:
+        names = [-1, *names]
+
+    yield from (data[start + 1:end].rename(data.get(start, '').lstrip(">")) for start, end in
+                zip_longest(names, names[1:]))
+
+
+def get_background(path: str) -> pd.Series:
+    with open(path) as f:
+        data = pd.Series(f.readlines())
+
+    data = data.str.strip()
+    data = data[~data.str.startswith('>')]
+
+    return data
 
 
 if __name__ == "__main__":
@@ -42,10 +59,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("genelist", type=str, help="gene list in FASTA format")
     parser.add_argument("-A", "--annotated", type=str, help="annotated matches", default="annotated.pickle.gz")
+    parser.add_argument("-b", "--background", type=str, help="gene list to use as the background")
     parser.add_argument("-P", "--promoter", type=promoter_size, help="limit promoter size")
-    parser.add_argument("-p", "--p-value", type=probability, help="p-value cutoff")
+    parser.add_argument("-p", "--p-value", type=probability, help="p-value cutoff of match used")
     parser.add_argument("-a", "--alpha", type=probability, help="alpha for enrichment", default=0.05)
-    parser.add_argument("--hide-rejected", action="store_false", help="only display significant results")
+    parser.add_argument("--hide-rejected", action="store_true", help="only display significant results")
     parser.add_argument("-o", "--output", type=str, help="output folder name")
 
     group = parser.add_argument_group(title="limit search")
@@ -59,22 +77,23 @@ if __name__ == "__main__":
 
     annotated = pd.read_pickle(args.annotated)
 
-    if args.p_value:
+    if args.p_value is not None:
         annotated = annotated[annotated['p-value'] < args.p_value]
 
-    if args.promoter:
+    if args.promoter is not None:
         annotated = annotated[annotated['dist'] >= -args.promoter]
+
+    if args.background:
+        bg = get_background(args.background)
+        annotated = annotated.loc[annotated.index.isin(bg), :]
 
     if args.promoter_only:
         annotated = annotated[(annotated['stop'] - annotated['start'] + annotated['dist']) < 0]
-
-    if args.promoter_overlap:
+    elif args.promoter_overlap:
         annotated = annotated[annotated['dist'] < 0]
-
-    if args.gene_only:
+    elif args.gene_only:
         annotated = annotated[annotated['dist'] > 0]
-
-    if args.gene_overlap:
+    elif args.gene_overlap:
         annotated = annotated[(annotated['stop'] - annotated['start'] + annotated['dist']) > 0]
 
     ann_dedup = annotated.drop_duplicates('match_id')
@@ -83,7 +102,11 @@ if __name__ == "__main__":
     print("total matches: {0[0]:,d}\n".format(ann_dedup.shape), file=sys.stderr)
 
 
-    def get_list_enrichment(gene_list, alpha: float = 0.05, show_rejected: bool = True) -> pd.DataFrame:
+    def get_list_enrichment(gene_list: pd.Series, alpha: float = 0.05, hide_rejected: bool = False) -> pd.DataFrame:
+        print("{} genes in gene list {} are not part of the backgroud".format(
+            gene_list[~gene_list.isin(annotated.index)].shape[0], gene_list.name),
+            file=sys.stderr)
+
         list_cluster_dedup = annotated[annotated.index.isin(gene_list)].drop_duplicates('match_id')
         list_cluster_size = list_cluster_dedup.groupby('#pattern name').size()
 
@@ -98,7 +121,7 @@ if __name__ == "__main__":
                              axis=1).fillna(0).apply(cluster_fisher, axis=1).sort_values()
         reject, adj_p = fdrcorrection(p_values, alpha=alpha, is_sorted=True)
 
-        if not show_rejected:
+        if hide_rejected:
             p_values = p_values[reject]
             adj_p = adj_p[reject]
 
@@ -107,13 +130,13 @@ if __name__ == "__main__":
 
 
     if args.output:
-        os.mkdir(args.output)
+        os.makedirs(args.output)
 
     for test_list in get_lists(args.genelist):
-        result = get_list_enrichment(test_list, alpha=args.alpha, show_rejected=args.hide_rejected)
+        result = get_list_enrichment(test_list, alpha=args.alpha, hide_rejected=args.hide_rejected)
         if args.output:
             result.to_csv(os.path.join(args.output, "{}.csv".format(test_list.name)))
         else:
             print(test_list.name)
             result.to_csv(sys.stdout, sep="\t")
-            print("-" * (width if width <= 80 else 80))
+            print("-" * min(width, 80))
